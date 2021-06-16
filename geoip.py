4e9b5842401e6b6c9aee19afb0fc0291df7218ca
WindexProjects <60302058+WindexProjects@users.noreply.github.com>
2021-06-16 03:59:28 -0500

    Create geoip.py

---
+++ geoip.py
@@ -0,0 +1,13 @@
+import requests
+from colorama import Fore, Back, Style, init 
+init(convert=true)
+geoip = input("Enter IP Address: ")
+    geolocate = requests.get("https://extreme-ip-lookup.com/json/" + str(geoip))
+    if (geolocate.status_code == 404):
+        print(Fore.RED + "[-] Could Not Geolocate, (Error 404)..")
+    if (geolocate.status_code == 200):
+        print(Fore.GREEN + "[+] Geolocating Successful!")
+        print(geolocate.text)
+    else:
+        print(Fore.YELLOW + "[?] Error Code: ", geolocate.status_code, "Could not Geolocate!")
+   #May Add support for saving the Results in a .txt file later :)
