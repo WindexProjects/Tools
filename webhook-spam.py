d23267298939cb89fa197e6033514ced9c342f75
WindexProjects <60302058+WindexProjects@users.noreply.github.com>
2021-06-21 20:21:01 -0500

    Updated Interface to look better
    Changed old "Webhook Spammer" into "Webhook Fucker" GUI

--- webhook-spam.py
+++ webhook-spam.py
@@ -1,12 +1,17 @@
 import requests
 from colorama import Fore, Back, Style, init
 init(convert=True)
-print(Fore.GREEN + Style.BRIGHT + "╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═")  
-print(Fore.YELLOW + "║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗") 
-print(Fore.MAGENTA + "╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩")
-print(Fore.BLUE + "╔═╗╔═╗╔═╗╔╦╗╔╦╗╔═╗╦═╗")
-print(Fore.CYAN + "╚═╗╠═╝╠═╣║║║║║║║╣ ╠╦╝")  
-print(Fore.LIGHTRED_EX + "╚═╝╩  ╩ ╩╩ ╩╩ ╩╚═╝╩╚═")
+print(Fore.YELLOW + "█     █░▓█████  ▄▄▄▄    ██░ ██  ▒█████   ▒█████   ██ ▄█▀     █████▒█    ██  ▄████▄   ██ ▄█▀▓█████  ██▀███")
+print(Fore.GREEN + "▓█░ █ ░█░▓█   ▀ ▓█████▄ ▓██░ ██▒▒██▒  ██▒▒██▒  ██▒ ██▄█▒    ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒")
+print(Fore.RED + "▒█░ █ ░█ ▒███   ▒██▒ ▄██▒██▀▀██░▒██░  ██▒▒██░  ██▒▓███▄░    ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒")
+print(Fore.MAGENTA+"░█░ █ ░█ ▒▓█  ▄ ▒██░█▀  ░▓█ ░██ ▒██   ██░▒██   ██░▓██ █▄    ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  ")
+print(Fore.BLUE + "░░██▒██▓ ░▒████▒░▓█  ▀█▓░▓█▒░██▓░ ████▓▒░░ ████▓▒░▒██▒ █▄   ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒")
+print(Fore.YELLOW + "░ ▓░▒ ▒  ░░ ▒░ ░░▒▓███▀▒ ▒ ░░▒░▒░ ▒░▒░▒░ ░ ▒░▒░▒░ ▒ ▒▒ ▓▒    ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░")
+print(Fore.GREEN + "  ▒ ░ ░   ░ ░  ░▒░▒   ░  ▒ ░▒░ ░  ░ ▒ ▒░   ░ ▒ ▒░ ░ ░▒ ▒░    ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░")
+print(Fore.RED + "  ░   ░     ░    ░    ░  ░  ░░ ░░ ░ ░ ▒  ░ ░ ░ ▒  ░ ░░ ░     ░ ░    ░░░ ░ ░ ░        ░ ░░ ░    ░     ░░   ░ ")
+print(Fore.MAGENTA + "    ░       ░  ░ ░       ░  ░  ░    ░ ░      ░ ░  ░  ░                ░     ░ ░      ░  ░      ░  ░   ░     ")
+print(Fore.BLUE + "                      ░                                                     ░                               ")
+print(Fore.RED + " ")
 webhook = input("Enter Webhook: ") #Only works for discord webhooks
 spam = input("Enter amount to Spam: ")
 message = input("Message to Spam: ")
